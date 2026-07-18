0d6f5a985bdcdd1692925a1c96940332d57aea1e
chibwesamuel <smchibwe@yahoo.com>
2026-07-18 16:00:12 +0200

    Add roles and permissions foundation

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,12 @@
 from app.models.user import User
+from app.models.role import Role
+from app.models.permission import Permission
+from app.models.role_permission import role_permissions
+
 
 __all__ = [
     "User",
-    ]
+    "Role",
+    "Permission",
+    "role_permissions",
+]
